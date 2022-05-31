bb24b2de97b07ef107bdf5ebbcbc084c4e116b45
ноут <vovazakharov2@gmail.com>
2022-05-31 22:41:54 +0300

    changed fill_db

--- habr_proj/mainapp/management/commands/fill_db.py
+++ habr_proj/mainapp/management/commands/fill_db.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from faker import Faker
 
 from authapp.models import HabrUser
 
+fake = Faker()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -12,3 +14,13 @@ class Command(BaseCommand):
             email=settings.ADMIN_EMAIL,
             password=settings.ADMIN_PASSWORD
         )
+        for i in range(int(input('Введите количество пользователей: '))):
+            user = HabrUser.objects.create(
+                username=fake.name(),
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                email=fake.email(),
+                birthday=fake.date_of_birth(),
+                about_me=fake.sentence(nb_words=10, variable_nb_words=True)
+            )
+            user.save()
