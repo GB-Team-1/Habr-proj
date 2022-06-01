4dbfb48f629a45681d604b2f33e7049e190ce8b3
ноут <vovazakharov2@gmail.com>
2022-06-01 23:19:26 +0300

    changed fill_db post.models

--- habr_proj/mainapp/management/commands/fill_db.py
+++ habr_proj/mainapp/management/commands/fill_db.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from faker import Faker
 
 from authapp.models import HabrUser
+from posts.models import PostCategory
 
 fake = Faker()
 
@@ -14,13 +15,8 @@ class Command(BaseCommand):
             email=settings.ADMIN_EMAIL,
             password=settings.ADMIN_PASSWORD
         )
-        for i in range(int(input('Введите количество пользователей: '))):
-            user = HabrUser.objects.create(
-                username=fake.name(),
-                first_name=fake.first_name(),
-                last_name=fake.last_name(),
-                email=fake.email(),
-                birthday=fake.date_of_birth(),
-                about_me=fake.sentence(nb_words=10, variable_nb_words=True)
-            )
-            user.save()
+        PostCategory.objects.all().delete()
+        PostCategory.objects.create(name='design').save()
+        PostCategory.objects.create(name='web').save()
+        PostCategory.objects.create(name='mobile').save()
+        PostCategory.objects.create(name='marketing').save()
--- habr_proj/posts/models.py
+++ habr_proj/posts/models.py
@@ -2,16 +2,5 @@ from django.db import models
 
 
 class PostCategory(models.Model):
-    DESIGN = 'DS'
-    WEB = 'WB'
-    MOBILE = 'MB'
-    MARKETING = 'MK'
-    CATEGORIES = (
-        (DESIGN, 'дизайн'),
-        (WEB, 'веб-разработка'),
-        (MOBILE, 'мобильная разработка'),
-        (MARKETING, 'маркетинг'),
-    )
-
-    name = models.CharField(verbose_name="категория", max_length=2, choices=CATEGORIES)
+    name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
