ee9b665388fd35ac69b70db9e186495a83abbd1a
Sorulai <57193937+Sorulai@users.noreply.github.com>
2022-06-02 14:39:25 +0300

    add postcategoryadmin, add migrations, fix fill_db [issue#fix]

--- habr_proj/posts/models.py
+++ habr_proj/posts/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class PostCategory(models.Model):
-    name = models.CharField(max_length=64, unique=True)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=64, unique=True, verbose_name='Название')
+    description = models.TextField(blank=True, verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
