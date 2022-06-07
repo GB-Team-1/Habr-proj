5ae965d98df1be5c0c242ad1c9c28fabafa2468a
Sorulai <57193937+Sorulai@users.noreply.github.com>
2022-06-07 20:26:35 +0300

    update posts templates, add publish view[issue#16]

--- habr_proj/posts/forms.py
+++ habr_proj/posts/forms.py
@@ -11,7 +11,10 @@ class PostCreateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            if field_name == 'is_published':
+                field.widget.attrs['class'] = 'colorinput-color bg-primary'
+            else:
+                field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
 
 
